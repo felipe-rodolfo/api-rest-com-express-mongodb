f827799eeb8c8aac301c39272bd30d998a5f5d95
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2023-07-16 11:10:56 -0300

    método post e get

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import livros from './models/Livro.js';
+import routes from './routes/index.js';
 
 db.on("error", console.log.bind(console, 'Erro de conexão'));
 db.once("open", () => {
@@ -11,17 +12,7 @@ const app = express();
 
 app.use(express.json());
 
-// const livros = [
-//     {id: 1, "titulo": "Senhor dos Aneis"},
-//     {id: 2, "título": "O Hobbit"}
-// ]
-
-app.get('/livros', (req, res) => {
-    livros.find((err, livros) => {
-        res.status(200).json(livros)
-    });
-    
-})
+routes(app);
 
 app.post('/livros', (req, res) => {
     livros.push(req.body);
