ee40a114ca6662a38d9eb5ccec29e4be43790562
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2023-07-18 22:48:41 -0300

    criando consultas diferenciadas

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -2,7 +2,9 @@ import livros from '../models/Livro.js';
 
 class LivroController {
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
+        livros.find()
+        .populate('autor')
+        .exec((err, livros) => {
             res.status(200).json(livros)
         });
     }
@@ -10,7 +12,9 @@ class LivroController {
     static listarLivroPorID = (req, res) => {
         const id = req.params.id;
 
-        livros.findById(id, (err, livros) => {
+        livros.findById(id)
+            .populate('autor', 'nome')
+            .exec((err, livros) => {
             if(err){
                 res.status(400).send({message: `${err.message} - Id do livro não localizado`})
             } else {
@@ -52,6 +56,14 @@ class LivroController {
             } else {
                 res.status(500).send({message: err.message})
             }
+        })
+    }
+
+    static listarLivroPorEditora = (req, res) =>{
+        const editora = req.query.editora
+
+        livros.find({'editora': editora}, {}, (err, livros) => {
+            res.status(200).send(livros);
         })
     }
       
