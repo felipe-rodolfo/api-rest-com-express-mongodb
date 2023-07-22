c5519446284d9e7c43e8067679ccdfdf55bb941a
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2023-07-22 14:05:17 -0300

    validacao personalizada

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,14 +1,28 @@
 import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema(
-    {
-        id: {type: String},
-        titulo: {type: String, required: true},
-        autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true},
-        editora: {type: String, required: true},
-        numeroPaginas: {type: Number},
-    }
+  {
+    id: {type: String},
+    titulo: {type: String, required: true},
+    autor: {type: mongoose.Schema.Types.ObjectId, ref: "autores", required: true},
+    editora: {
+      type: String, 
+      required: true,
+      enum: {
+        values:  ["Casa do código", "Alura"],
+        message: "A editora {VALUE} não é um valor permitido."
+      }
+    },
+    numeroPaginas: {
+      type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        }
+      }
+    },
+  }
 );
 
-const livros = mongoose.model('livros', livroSchema);
+const livros = mongoose.model("livros", livroSchema);
 export default livros;
