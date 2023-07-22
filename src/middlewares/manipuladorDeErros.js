841dd96e6ea16e362c70f9fe5575e9cb372d14b4
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2023-07-21 23:03:19 -0300

    erros de validacao

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -4,6 +4,11 @@ import mongoose from "mongoose";
 function manipuladorDeErros (error, req, res, next) {
   if (error instanceof mongoose.Error.CastError) {
     res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos."});
+  } else if(error instanceof mongoose.Error.ValidationError){
+    const mensagensErro = Object.values(error.errors)
+      .map(erro => erro.message)
+      .join("; ");
+    res.status(400).send({message: `Houve um erro de validação de dados. ${mensagensErro}`});
   } else { 
     res.status(500).send({message: "Erro interno de servidor."});
   }
